7cf4bba7cd84a070618e0f3659d82bef868efda2
2022100ryoohanseo <119025557+2022100ryoohanseo@users.noreply.github.com>
2022-11-25 14:03:18 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,35 @@
 let mySound;
+let button;
 
 function setup()
 {
+    createCanvas(200,200);
 soundFormats('mp3', 'ogg');
 mySound=loadSound('lemon', loadMusic);
+button = crateButton('play');
+button.mousePressed(togglePlaying);
+background(51);
 //mySound.play();
 }
 
-function loadMusic(){
-    mySound.play();
+function togglePlaying(){
+    if(!song.isPlaying()){
+        song.play();
+        song.setVolume(0.3);
+        buttom.html('pause');
+    }else{
+        song.stop();
+        button.html('play');
+    }
 }
+
+function loadMusic(){
+    console.log("loaded");
+}
+
 
 function draw(){
     fill(255,0,0);
-    
+    ellipse();
 
 }
